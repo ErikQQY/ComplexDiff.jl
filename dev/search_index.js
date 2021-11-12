ac141046e9a8a7e3c207ac8de04710fdbb102117
Documenter.jl <documenter@juliadocs.github.io>
2021-11-12 14:34:10 +0000

    build based on ead09ab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ComplexDiff","category":"page"},{"location":"#ComplexDiff","page":"Home","title":"ComplexDiff","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ComplexDiff.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ComplexDiff]","category":"page"}]
+}
